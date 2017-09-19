a4781fd6c6e221c1c7f53076658c2dbbda1fa598
kentongray <kentongray@gmail.com>
2017-09-19 15:12:06 -0500

    Update Notifications.ts

--- app/Notifications.ts
+++ app/Notifications.ts
@@ -13,7 +13,7 @@ export interface Notification {
 const notifications: { houston: [Notification] } = {
   "houston": [{
     title: 'Harvey Updates',
-    text: 'Recycling, Heavy and Tree pickup is suspended until further notice. \nFor information about recovery efforts and potential schedule changes tap here to view the latest information.',
+    text: 'Recycling, Yard, Heavy and Tree pickup is suspended until further notice. \nFor the latest information about recovery efforts and potential schedule changes tap here.',
     link: 'http://www.houstontx.gov/solidwaste/',
     urgent: true,
     expiresOn: moment("2017-11-01")
