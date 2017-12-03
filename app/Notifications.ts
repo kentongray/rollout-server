4c90fd063b3c333d2639aaebddea29eccba55539
kentongray <kentongray@gmail.com>
2017-12-02 22:49:57 -0600

    update notification

--- app/Notifications.ts
+++ app/Notifications.ts
@@ -12,19 +12,13 @@ export interface Notification {
 // someday it would be nice to have a backend for
 const notifications: { houston: [Notification] } = {
   "houston": [{
-    title: 'Recycling is Returning (Soon)!',
-    text: 'Normal recycling schedules are coming back Monday, November 13th. Tap here for the latest news.',
+    title: 'Recycling is Back!',
+    text: 'Normal recycling schedules are back. Tap here for the latest news.',
     link: 'http://www.houstontx.gov/solidwaste/',
     urgent: true,
     expiresOn: moment("2018-01-01")
-    },
-    {
-    title: 'Harvey Updates',
-    text: 'Recycling, Yard, Heavy and Tree pickup is suspended until further notice. \nFor the latest information about recovery efforts and potential schedule changes tap here.',
-    link: 'http://www.houstontx.gov/solidwaste/',
-    urgent: true,
-    expiresOn: moment("2017-11-01")
-  }]
+    }
+    ]
 };
 
 const houstonNotifications = notifications['houston'];
