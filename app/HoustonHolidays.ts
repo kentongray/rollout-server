a36274b5ccdcaee4f561226588a7fb7939a1afc2
Kenton <kentongray@gmail.com>
2020-11-08 15:28:02 -0600

    Update holiday

--- app/HoustonHolidays.ts
+++ app/HoustonHolidays.ts
@@ -1,5 +1,5 @@
 import * as moment from 'moment';
 // http://www.houstontx.gov/solidwaste/holiday.html
 export const HOLIDAYS = [
-  '2019-12-24', '2019-12-25', '2019-12-26','2019-12-31','2020-01-01','2020-01-02'
+  '2020-11-11', '2020-11-26', '2020-11-26', '2020-12-24', '2020-12-25', '2020-01-01', '2020-01-02'
 ].map((d) => moment(d, ' "YYYY-MM-DD"'));
