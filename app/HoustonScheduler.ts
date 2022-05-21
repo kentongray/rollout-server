7b5f51add82feacdbc981aca24f6fb3f988fd9af
Kenton <kentongray@gmail.com>
2022-05-21 16:24:29 -0500

    moving to a different server as the current one is down :(

--- app/HoustonScheduler.ts
+++ app/HoustonScheduler.ts
@@ -1,13 +1,14 @@
 import * as moment from "moment";
 import {Moment} from "moment";
 import * as _ from "lodash";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import {HOLIDAYS} from "./HoustonHolidays";
 import {EventInfo, PickupDay, Scheduler} from "./Scheduler";
 
 //interfaces for different coordinate types, prefer latitude longitude
 export interface PosCoords {
-  latitude: number,longitude: number
+    latitude: number,
+    longitude: number
 }
 
 /**
@@ -26,157 +27,165 @@ export interface PosCoords {
  * http://mycity.houstontx.gov/cohgis/rest/services/SWD/SolidWaste_wm/MapServer/4/query?&geometry=%7B%22y%22%3A%2229.7982722%22%2C%22x%22%3A%22-95.3736702%22%2C%22spatialReference%22%3A%7B%22wkid%22%3A4326%7D%7D&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&returnGeometry=false&outSR=102100&f=json&outFields=SERVICE_DAY
  **/
 export class HoustonScheduler implements Scheduler {
-  numberOfDays: number;
-  pickupDays: PickupDay;
-  holidays = HOLIDAYS;
-  whenLoaded: Promise<void>;
+    numberOfDays: number;
+    pickupDays: PickupDay;
+    holidays = HOLIDAYS;
+    whenLoaded: Promise<void>;
 
-  static getDayIndex(dayStr) {
-    return moment(dayStr, 'dddd').day()
-  }
-
-  readonly mapNumbers = [3];
-
-  readonly mapServer = 'https://mycity2.houstontx.gov/pubgis01/rest/services/EGIS/HOUSTON_CITYINFO/MapServer/3';
-
-  /**
-   * Initializes the obj with event data
-   * @param pos
-   * @param numberOfDays
-   */
-  constructor(pos: PosCoords, numberOfDays: number = 60) {
-    this.numberOfDays = numberOfDays;
-    const esriPos = {y: (<PosCoords> pos).latitude, x: (<PosCoords> pos).longitude, spatialReference: {'wkid': 4326}};
-
-
-    const params = {
-      geometry: JSON.stringify(esriPos),
-      geometryType: 'esriGeometryPoint',
-      spatialRel: 'esriSpatialRelIntersects',
-      returnGeometry: 'false',
-      f: 'json',
-    };
-
-    //https://mycity2.houstontx.gov/pubgis01/rest/services/EGIS/HOUSTON_CITYINFO/MapServer/3/query?&geometry=%7B%22y%22%3A%2229.7982722%22%2C%22x%22%3A%22-95.3736702%22%2C%22spatialReference%22%3A%7B%22wkid%22%3A4326%7D%7D&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=SWDGarbageDay%2CSWDRecyclingDAY%2CSWDHeavyTrashDAY&returnGeometry=false&f=pjson
-
-    const paramStr = Object.keys(params).reduce((paramStr, key) => `${paramStr}&${key}=${encodeURIComponent(params[key])}`, '?');
-
-
-    const url = `${this.mapServer}/query${paramStr}&outFields=SWDGarbageDay,SWDRecyclingDAY,SWDHeavyTrashDAY`;
-    console.log(url);
-    const request = axios.request({method: 'GET', url, timeout: 15000, responseType: 'json'});
-
-    //get all relevant data
-    this.whenLoaded = request.then((results) => {
-      this.parseData(results);
-    });
-  }
-
-  /**
-   * Take results from COH API and turn them into something we can work with
-   * @param wasteData
-   * @param junkData
-   * @param recyclingData
-   * @returns {Array<any>}
-   */
-  parseData(results:AxiosResponse) {
-    //waste is one day a week
-    let wasteDay = -1;
-    //heavy trash pickup is in the form of #rd WEEKDAY
-    let junkWeekOfMonth = -1;
-    let junkDay = -1;
-    //recycling pickup is alternating weeks
-    let recyclingDay = -1;
-    let recyclingOnEvenWeeks = false;
-    
-    console.log(results.data);
-    if (this.isValidData(results.data)) {
-      const attributes = results.data.features[0].attributes;
-      wasteDay = HoustonScheduler.getDayIndex(attributes.SWDGarbageDay);
-      let junkPattern = attributes.SWDHeavyTrashDAY;
-      junkWeekOfMonth = parseInt(junkPattern.substr(0, 1));
-      junkDay = HoustonScheduler.getDayIndex(junkPattern.substr(junkPattern.indexOf(' ')));
-      let recyclingSchedule = attributes.SWDRecyclingDAY;
-      recyclingDay = HoustonScheduler.getDayIndex(recyclingSchedule.split('-')[0]);
-      // on even years A schedule is on odd weeks, vice versa on odd years
-      //http://www.houstontx.gov/solidwaste/Recycle_Cal.pdf
-      const aSchedule = recyclingSchedule.includes('-A');
-      recyclingOnEvenWeeks = aSchedule;
+    static getDayIndex(dayStr) {
+        return moment(dayStr, 'dddd').day()
     }
 
+    readonly mapNumbers = [3];
 
-    this.pickupDays = {wasteDay, junkWeekOfMonth, junkDay, recyclingDay, recyclingOnEvenWeeks};
-  }
+    readonly mapServer = 'https://mycity2.houstontx.gov/pubgis01/rest/services/SWM/SWM_Services/MapServer/';
 
-  isValidData(data) {
-    return data && data.features && data.features.length && data.features[0].attributes;
-  }
+    /**
+     * Initializes the obj with event data
+     * @param pos
+     * @param numberOfDays
+     */
+    constructor(pos: PosCoords, numberOfDays: number = 60) {
+        this.numberOfDays = numberOfDays;
+        const esriPos = {y: (<PosCoords>pos).latitude, x: (<PosCoords>pos).longitude, spatialReference: {'wkid': 4326}};
 
-  isWasteDay(day) {
-    return day.day() == this.pickupDays.wasteDay;
-  }
 
-  //used for both trash/and junk days
-  isHeavyDay(day) {
-    let dayInMonth = day.clone().startOf('month');
-    let occurances = 0;
-    while (occurances < this.pickupDays.junkWeekOfMonth) {
-      if (dayInMonth.day() == this.pickupDays.junkDay) {
-        occurances++;
-      }
-      dayInMonth.add(1, 'days');
+        const params = {
+            geometry: JSON.stringify(esriPos),
+            geometryType: 'esriGeometryPoint',
+            spatialRel: 'esriSpatialRelIntersects',
+            returnGeometry: 'false',
+            f: 'json',
+        };
+
+
+        //https://mycity2.houstontx.gov/pubgis01/rest/services/EGIS/HOUSTON_CITYINFO/MapServer/3/query?&geometry=%7B%22y%22%3A%2229.7982722%22%2C%22x%22%3A%22-95.3736702%22%2C%22spatialReference%22%3A%7B%22wkid%22%3A4326%7D%7D&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=SWDGarbageDay%2CSWDRecyclingDAY%2CSWDHeavyTrashDAY&returnGeometry=false&f=pjson
+
+        const paramStr = Object.keys(params).reduce((paramStr, key) => `${paramStr}&${key}=${encodeURIComponent(params[key])}`, '?');
+
+        const mapLayers = {1: 'DAY', 2: 'SERVICE_DAY', 3: 'SERVICE_DA'};
+        const requests = Object.keys(mapLayers).map(layerId => {
+            return `${this.mapServer}${layerId}/query${paramStr}&outFields=${mapLayers[layerId]}`
+        }).map(url => axios.request({method: 'GET', url, timeout: 15000, responseType: 'json'}))
+
+        //get all relevant data
+        this.whenLoaded = Promise.all(requests)
+            .then(requests => requests.reduce((acc, r) => ({...acc, ...r.data.features[0].attributes}), {})
+            ).then((results) => {
+                console.log(results);
+                this.parseData({
+                    garbage: results.DAY,
+                    recycling: results.SERVICE_DAY,
+                    junk: results.SERVICE_DA,
+                });
+            });
     }
-    //offset the last day added (ew)
-    dayInMonth.add(-1, 'days');
-    return dayInMonth.isSame(day, 'day');
-  }
 
-  isTreeDay(day) {
-    return !this.isEvenMonth(day) && this.isHeavyDay(day);
-  }
+    /**
+     * Take results from COH API and turn them into something we can work with
+     * @param wasteData
+     * @param junkData
+     * @param recyclingData
+     * @returns {Array<any>}
+     */
+    parseData(results: { garbage: string, recycling: string, junk: string }) {
+        //waste is one day a week
+        let wasteDay = -1;
+        //heavy trash pickup is in the form of #rd WEEKDAY
+        let junkWeekOfMonth = -1;
+        let junkDay = -1;
+        //recycling pickup is alternating weeks
+        let recyclingDay = -1;
+        let recyclingOnEvenWeeks = false;
 
-  isJunkDay(day) {
-    return this.isEvenMonth(day) && this.isHeavyDay(day);
-  }
+        console.log(results);
+        if (this.isValidData(results)) {
 
-  isEvenMonth(day) {
-    return (day.month() + 1) % 2 == 0;
-  }
-
-  isRecyclingDay(day) {
-    //recycling schedule A occurs every other week (starting at second week)
-    let isEvenWeek = day.weeks() % 2 == 1;
-    let isThisWeek = (this.pickupDays.recyclingOnEvenWeeks && isEvenWeek) || (!this.pickupDays.recyclingOnEvenWeeks && !isEvenWeek);
-    return isThisWeek && day.day() == this.pickupDays.recyclingDay;
-  }
-
-  isPossibleHoliday(day) {
-    return _.some(this.holidays, (d) => d.isSame(day, 'day'))
-  }
-
-  getCategoriesForDay(day) {
-    let eventsForDay = {
-      waste: this.isWasteDay(day),
-      junk: this.isJunkDay(day),
-      tree: this.isTreeDay(day),
-      recycling: this.isRecyclingDay(day)
-    };
-    //group filter out empty days
-    return _.toPairs(eventsForDay).filter((category) => category[1]).map((category) => category[0]);
-  }
-
-  getUpcomingEvents(numberOfDays = 60): Promise<EventInfo[]> {
-    return this.whenLoaded.then(() => {
-      let day: Moment = moment().startOf('day');
-      let groupEvents: (day) => EventInfo = (day) => {
-        return {
-          day: day, categories: this.getCategoriesForDay(day), possibleHoliday: this.isPossibleHoliday(day)
+            wasteDay = HoustonScheduler.getDayIndex(results.garbage);
+            let junkPattern = results.junk;
+            junkWeekOfMonth = parseInt(junkPattern.substr(0, 1));
+            junkDay = HoustonScheduler.getDayIndex(junkPattern.substr(junkPattern.indexOf(' ')));
+            let recyclingSchedule = results.recycling;
+            recyclingDay = HoustonScheduler.getDayIndex(recyclingSchedule.split('-')[0]);
+            // on even years A schedule is on odd weeks, vice versa on odd years
+            //http://www.houstontx.gov/solidwaste/Recycle_Cal.pdf
+            const aSchedule = recyclingSchedule.indexOf('-A') !== -1;
+            recyclingOnEvenWeeks = aSchedule;
         }
-      };
-      return _.range(0, numberOfDays).map((i) => day.clone().add(i, 'days')).map(groupEvents)
-        .filter((event) => event.categories.length)
-    });
-  }
+
+
+        this.pickupDays = {wasteDay, junkWeekOfMonth, junkDay, recyclingDay, recyclingOnEvenWeeks};
+    }
+
+    isValidData(data) {
+        return true;
+    }
+
+    isWasteDay(day) {
+        return day.day() == this.pickupDays.wasteDay;
+    }
+
+    //used for both trash/and junk days
+    isHeavyDay(day) {
+        let dayInMonth = day.clone().startOf('month');
+        let occurances = 0;
+        while (occurances < this.pickupDays.junkWeekOfMonth) {
+            if (dayInMonth.day() == this.pickupDays.junkDay) {
+                occurances++;
+            }
+            dayInMonth.add(1, 'days');
+        }
+        //offset the last day added (ew)
+        dayInMonth.add(-1, 'days');
+        return dayInMonth.isSame(day, 'day');
+    }
+
+    isTreeDay(day) {
+        return !this.isEvenMonth(day) && this.isHeavyDay(day);
+    }
+
+    isJunkDay(day) {
+        return this.isEvenMonth(day) && this.isHeavyDay(day);
+    }
+
+    isEvenMonth(day) {
+        return (day.month() + 1) % 2 == 0;
+    }
+
+    isRecyclingDay(day) {
+        //recycling schedule A occurs every other week (starting at second week)
+        let isEvenWeek = day.weeks() % 2 == 1;
+        let isThisWeek = (this.pickupDays.recyclingOnEvenWeeks && isEvenWeek) || (!this.pickupDays.recyclingOnEvenWeeks && !isEvenWeek);
+        return isThisWeek && day.day() == this.pickupDays.recyclingDay;
+    }
+
+    isPossibleHoliday(day) {
+        return _.some(this.holidays, (d) => d.isSame(day, 'day'))
+    }
+
+    getCategoriesForDay(day) {
+        let eventsForDay = {
+            waste: this.isWasteDay(day),
+            junk: this.isJunkDay(day),
+            tree: this.isTreeDay(day),
+            recycling: this.isRecyclingDay(day)
+        };
+        //group filter out empty days
+        return _.toPairs(eventsForDay).filter((category) => category[1]).map((category) => category[0]);
+    }
+
+    getUpcomingEvents(numberOfDays = 60): Promise<EventInfo[]> {
+        return this.whenLoaded.then(() => {
+            let day: Moment = moment().startOf('day');
+            let groupEvents: (day) => EventInfo = (day) => {
+                return {
+                    day: day, categories: this.getCategoriesForDay(day), possibleHoliday: this.isPossibleHoliday(day)
+                }
+            };
+            return _.range(0, numberOfDays).map((i) => day.clone().add(i, 'days')).map(groupEvents)
+                .filter((event) => event.categories.length)
+        });
+    }
 
 
 }
